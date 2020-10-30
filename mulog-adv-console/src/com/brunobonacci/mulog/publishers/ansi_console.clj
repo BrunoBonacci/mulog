9d14f116bde2e9235d19038b97499b9242e35984
Simion Belea <simion.belea@rvu.co.uk>
2020-10-30 16:59:11 +0000

    ansi console

---
+++ mulog-adv-console/src/com/brunobonacci/mulog/publishers/ansi_console.clj
@@ -0,0 +1,97 @@
+ (ns com.brunobonacci.mulog.publishers.ansi-console
+   (:require [com.brunobonacci.mulog.buffer :as rb]
+             [com.brunobonacci.mulog.publishers.helper.clansi :as ansi]))
+
+(defn colorize
+  [thing color]
+  (ansi/style thing color))
+
+(defn colorize-item
+  [item color]
+  (reduce-kv (fn [acc k v]
+               (assoc acc
+                      (colorize k color)
+                      (colorize v color)))
+             {}
+             item))
+
+(def formatters
+  (atom {}))
+
+(defn register-formatters
+  [formatter-config]
+  (reset! formatters formatter-config))
+
+(defn find-format
+  [rules [key val]]
+  (->> rules
+       (partition 2)
+       (keep
+        (fn [[match? fmt]]
+          (when (match? (hash-map key val))
+            fmt)))))
+
+(defn find-all-formats
+  [rules entry]
+  (mapcat (partial find-format rules) entry))
+
+(defn extract-format
+  [format-type formats]
+  (->> formats
+       (keep
+        (fn [fmt]
+          (let [rule-format-key (-> fmt vals first)]
+            (case format-type
+              :pair (hash-map (-> fmt keys first)
+                              (get-in @formatters [rule-format-key]))
+              :event (get-in @formatters [rule-format-key format-type])
+              (throw (Exception. "Format type not supported"))))))
+       (into [])))
+
+(defn format-for
+  [entry rules format-type]
+  (->> entry
+       (find-all-formats rules)
+       (extract-format format-type)))
+
+(defn entry-format
+  [entry rules]
+  (->> (format-for entry rules :event)
+       (cons (get-in @formatters [:default-formatter :event]))
+       last))
+
+(defn pair-formats
+  [entry rules]
+  (->> (format-for entry rules :pair)
+       (filter #(:pair (-> % vals first)))
+       (apply merge)))
+
+(deftype AdvancedConsolePublisher
+         [config buffer]
+  com.brunobonacci.mulog.publisher.PPublisher
+  (agent-buffer [_]
+    buffer)
+
+  (publish-delay [_]
+    200)
+
+  (publish [_ buffer]
+    (doseq [item (map second (rb/items buffer))
+            :let [rules (:format config)
+                  event-fmt (entry-format item rules)
+                  pair-formats (pair-formats item rules)
+                  pair-keys (keys pair-formats)
+                  event-without-pair-fmt (apply dissoc item pair-keys)
+                  event-pairs (select-keys item pair-keys)]]
+      (println (->> event-pairs
+                    (map (fn [[k v]]
+                           (colorize-item (hash-map k v)
+                                          (get-in pair-formats [k :pair]))))
+                    (apply merge
+                           (colorize-item event-without-pair-fmt event-fmt)))))
+    (flush)
+    (rb/clear buffer)))
+
+(defn advanced-console-publisher
+  [config]
+  (AdvancedConsolePublisher. config (rb/agent-buffer 10000)))
