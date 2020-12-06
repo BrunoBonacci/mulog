6603ed31f3458f2c285f0bce480ef73d56295ade
Tovieye Ozi <tozi@erltelecoms.com>
2020-12-06 18:05:57 +0100

    swap cheshire for jsonista
    jsonista is reportedly faster than cheshire at json serialization

--- mulog-json/project.clj
+++ mulog-json/project.clj
@@ -11,7 +11,7 @@
 
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [com.brunobonacci/mulog #=(ver)]
-                 [cheshire "5.10.0"]]
+                 [metosin/jsonista "0.2.7"]]
 
   :global-vars {*warn-on-reflection* true}
 
--- mulog-json/src/com/brunobonacci/mulog/common/json.clj
+++ mulog-json/src/com/brunobonacci/mulog/common/json.clj
@@ -1,38 +1,41 @@
 (ns com.brunobonacci.mulog.common.json
-  (:require [cheshire.core :as json]
-            [cheshire.generate :as gen]
+  (:require [jsonista.core :as json]
             [com.brunobonacci.mulog.utils :as ut]))
 
 
 
 ;;
 ;; Add Flake encoder to JSON generator
-;;
-(gen/add-encoder com.brunobonacci.mulog.core.Flake
-  (fn [x ^com.fasterxml.jackson.core.JsonGenerator json]
-    (gen/write-string json ^String (str x))))
-
-
-
-;;
 ;; Add Exception encoder to JSON generator
 ;;
-(gen/add-encoder java.lang.Throwable
-  (fn [x ^com.fasterxml.jackson.core.JsonGenerator json]
-    (gen/write-string json ^String (ut/exception-stacktrace x))))
+(def encoders {java.lang.Throwable (fn [x  gen]
+                                     (.writeString gen ^String (ut/exception-stacktrace x)))
+               com.brunobonacci.mulog.core.Flake (fn [x  gen]
+                                                   (.writeString gen ^String (str x)))})
 
 
+(def object-mapper-options {:date-format "yyyy-MM-dd'T'HH:mm:ss.SSSX"
+                            :encoders encoders})
+
+
+(def plain-mappper (json/object-mapper object-mapper-options))
+
+
+(def pretty-mappper (json/object-mapper
+                        (assoc object-mapper-options :pretty? true)))
 
 (defn to-json
   "It takes a map and return a JSON encoded string of the given map data."
   ([m]
-   (json/generate-string m {:date-format "yyyy-MM-dd'T'HH:mm:ss.SSSX"}))
-  ([m {:keys [pretty?] :as cfg}]
-   (json/generate-string m {:date-format "yyyy-MM-dd'T'HH:mm:ss.SSSX" :pretty pretty?})))
+   (json/write-value-as-string m plain-mappper))
+  ([m {:keys [pretty?]}]
+   (json/write-value-as-string m (if pretty? pretty-mappper plain-mappper))))
 
 
 
 (defn from-json
   "Parses a JSON encoded string `s` into the representing data"
-  [s & {:keys [keywordize] :or {keywordize true}}]
-  (json/parse-string s keywordize))
+  ([s]
+   (json/read-value s json/keyword-keys-object-mapper))
+  ([s {:keys [keywordize]}]
+   (json/read-value s (if keywordize json/keyword-keys-object-mapper json/default-object-mapper))))
