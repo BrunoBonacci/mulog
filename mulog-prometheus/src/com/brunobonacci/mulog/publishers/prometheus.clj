72ee08fc1448a60d74def2c0c15153e72ab1b1f4
胡雨軒 Петр <piotr-yuxuan-github@2bst.fr>
2021-06-08 11:44:04 +0100

    [prometheus] Fix name shadowing issue withe registry(#68)
    * Rename variable that shadows type fn
    
    Attempt at avoiding the exception reported here: https://github.com/BrunoBonacci/mulog/issues/67#issue-906482305
    
    * Better name for collector-registry
    
    Co-authored-by: piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>

--- mulog-prometheus/src/com/brunobonacci/mulog/publishers/prometheus.clj
+++ mulog-prometheus/src/com/brunobonacci/mulog/publishers/prometheus.clj
@@ -204,7 +204,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (deftype PrometheusPublisher
-         [config buffer registry transform push-ts]
+         [config buffer collector-registry transform push-ts]
 
   com.brunobonacci.mulog.publisher.PPublisher
   (agent-buffer [_]
@@ -223,13 +223,13 @@
 
   com.brunobonacci.mulog.publishers.prometheus.ReadRegistry
   (registry [_]
-    (registry registry))
+    (registry collector-registry))
 
   (write-out [_ out]
-    (write-out registry out))
+    (write-out collector-registry out))
 
   (write-str [_]
-    (write-str registry))
+    (write-str collector-registry))
 
   java.io.Closeable
   (close [_]))
