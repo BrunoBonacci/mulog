f7be70fc978631d8c97bc70cf9ffaa0258a77ed6
Bruno Bonacci <bruno.bonacci@gmail.com>
2024-12-13 18:45:41 +0000

    [json] bump json library (charred) version

--- mulog-json/project.clj
+++ mulog-json/project.clj
@@ -11,15 +11,13 @@
 
   :dependencies [[org.clojure/clojure "1.12.0"]
                  [com.brunobonacci/mulog #=(ver)]
-                 [com.cnuernber/charred "1.010"]]
+                 [com.cnuernber/charred "1.034"]]
 
   :global-vars {*warn-on-reflection* true}
 
   :jvm-opts ["-server"]
 
-  :profiles {:dev {:dependencies [[midje "1.10.5"]
-                                  [org.clojure/test.check "1.1.1"]
-                                  [criterium "0.4.6"]]
+  :profiles {:dev {:dependencies [[midje "1.10.10"]]
                    :resource-paths ["dev-resources"]
                    :plugins      [[lein-midje "3.2.2"]]}}
 
